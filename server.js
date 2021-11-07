afff8e1c4a404a6f16c1ec0385bbf00f1a1b02ca
Maria Cassandra Gugol <mcmgugol@gmail.com>
2021-11-07 11:26:02 -0800

    fix folder structure

--- server.js
+++ server.js
@@ -3,6 +3,7 @@
 const express = require('express');
 const path = require('path');
 
+
 // Sets up the Express App
 
 const app = express();
@@ -14,11 +15,11 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
 // routes
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
-app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'notes.html')));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public','index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public','notes.html')));
 
 // display all notes
-app.get('/api/notes', (req, res) => res.json(notes));
+// app.get('/api/notes', (req, res) => res.json(notes));
 
 // display a single note
 
